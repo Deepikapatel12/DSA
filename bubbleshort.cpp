cc8a7c7def3a429978b6cf5b11304e498e11e8c7
Deepikapatel <deepikapatel5687@gmail.com>
2024-07-09 23:51:37 +0530

    bubble sort

--- bubbleshort.cpp
+++ bubbleshort.cpp
@@ -3,5 +3,37 @@ using namespace std;
 int main()
 {
  int a[5]={5,6,8,4,1};
+ int tmp;
+ int c=0;
+ int s=sizeof(a)/sizeof(a[0]);
+ for(int i=0;i<s;i++)
+ {
+     cout<<a[i]<<"\n";
+ }
+ //apply bubble sort
+ for(int i=0;i<s;i++)
+ {
+     for(int j=0;j<s-i-1;j++)
+     {
+         if(a[j]>a[j+1])
+         {
+             tmp=a[j];
+             a[j]=a[j+1];
+             a[j+1]=tmp;
+             c++;
+         }
+     }
+     if(c==0)
+     {
+         cout<<"Data is Already sorted\n";
+         break;
+     }
+ }
+ cout<<"After Bubble Sort\n";
+ for(int i=0;i<s;i++)
+ {
+     cout<<a[i]<<"\n";
+ }
+ cout<<"\ncounter hit="<<c;
 
 }
