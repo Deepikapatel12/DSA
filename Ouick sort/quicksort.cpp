6e6f83a5401b11833a9ec712dab4d39366b38bcf
Deepikapatel <deepikapatel5687@gmail.com>
2024-07-23 21:21:14 +0530

    Quick sort

--- Ouick sort/quicksort.cpp
+++ Ouick sort/quicksort.cpp
@@ -1,1 +1,59 @@
+#include<iostream>
+using namespace std;
+//select pivot element and left and right
+int part(int a[],int low, int high)
+{
+    int pvt=a[low];
+    int l=low+1;
+    int r=high;
+    int tmp;
+    do{
+        while(a[l]<pvt)
+        {
+            l++;
+        }
+        while(a[r]>pvt)
+        {
+            r--;
+        }
+        if(l<r)
+        {
+            tmp=a[r];
+            a[r]=a[l];
+            a[l]=tmp;
+        }
+    }
+    while(l<r);
+    tmp=a[low];
+    a[low]=a[r];
+    a[r]=tmp;
+    return r;
+}
 
+void qs(int a[],int low, int high)
+{
+    if(low<high)
+    {
+        int pvt=part(a,low,high);
+        qs(a,0,pvt-1);
+        qs(a,pvt+1,high);
+    }
+}
+
+void print(int a[], int s)
+{
+    for(int i=0;i<s;i++)
+    {
+        cout<<a[i]<<"\t";
+    }
+}
+
+int main()
+{
+    int a[]={2,5,3,4,10,9,8};
+    int s=sizeof(a)/sizeof(a[0]);
+    print (a,s);
+    qs (a,0,s-1);
+    cout<<"\n After Quick sort\n";
+    print(a,s);
+}
