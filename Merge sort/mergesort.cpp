759814518abd026644d55bf220915e598f259d6e
Deepikapatel <deepikapatel5687@gmail.com>
2024-07-25 22:21:23 +0530

    Merge sort

---
+++ Merge sort/mergesort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespacestd;
+void part(int a[], int low, int high)
+{
+    if(low<high)
+    {
+        int mid
+    }
+}
+void print(int a[], int s)
+{
+    for(int i=0;i<s;i++)
+    {
+        cout<<a[i]<<"\t";
+    }
+}
+int main()
+{
+     int a[]=
+}
