efc3328d99e807daf6abb10e482c11041528d8a6
Deepikapatel <deepikapatel5687@gmail.com>
2024-07-26 23:22:52 +0530

    Merge sort

--- Merge sort/mergesort.cpp
+++ Merge sort/mergesort.cpp
@@ -1,5 +1,41 @@
 #include<iostream>
 using namespacestd;
+void mrg(int a[], int low, int mid, int high)
+{
+    int i=low;
+    int j=mid+1;
+    int k=low;
+    int b[5];
+    while(i<=mid; && j<=high)
+    {
+        if(a[i]<a[j])
+        {
+            b[k]=a[i];
+            i++;
+        }
+        else{
+            b[k]=a[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<=mid)
+    {
+        b[k]=a[i];
+        i++;
+        k++;
+    }
+    while(j<=high)
+    {
+        b[k]=a[j];
+        j++;
+        k++;
+    }
+    for(int i=low;i<=high;i++)
+    {
+        a[i]=b[i];
+    }
+}
 void qs(int a[], int low, int high)
 {
     if(low<high)
