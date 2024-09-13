59c586de3ed4a402c99e4ff151a6ef8f5daced87
Deepikapatel <deepikapatel5687@gmail.com>
2024-09-13 22:22:04 +0530

    Litrals

---
+++ Litrals/caracter litrals/character.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ //char e='sumit';// character litral
+   // cout<<e<<"\n";
+     char e='sumit';// character litral
+    cout<<e<<"\n";
+    char f='p';
+    cout<<f<<"\n";
+    char g[]="sumit";
+    cout<<g<<"\n";
+    char h[6]="sumit";
+    cout<<h<<"\n";
+}
