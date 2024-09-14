e4294cb3bb0af4dcabe6b370117f1d8114972dc9
Deepikapatel <deepikapatel5687@gmail.com>
2024-09-14 10:31:30 +0530

    float litrals

---
+++ float litrals/floatlitral.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int c=034;//octal
+    cout<<c<<"\n";
+    int t=0x34;//hexadecimal
+    cout<<t<<"\n";
+    char e='sumit';// character litral
+    cout<<e<<"\n";
+    char f='p';
+    cout<<f<<"\n";
+    char g[]="sumit";
+    cout<<g<<"\n";
+    char h[6]="sumit";
+    cout<<h<<"\n";
+float d=98.34e-4;
+cout<<d<<"\n";
+float k=98.34e4;
+cout<<k<<"\n";
+}
