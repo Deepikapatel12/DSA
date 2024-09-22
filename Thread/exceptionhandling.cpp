37224f7cb0cc523ddb2b8e36276b5f95000a991e
Deepikapatel <deepikapatel5687@gmail.com>
2024-09-22 20:53:28 +0530

    exception handling

--- Thread/exceptionhandling.cpp
+++ Thread/exceptionhandling.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 using namespace std;
+//Exception : Abnormal condition
+//Exception Handling : Is used to avoid abnormal condition
 int main()
 {
+int a,b;
+cout<<"enter 2 numbers\n";
+cin>>a>>b;
+try{
+if(b==0)
+{
+    throw b;
+}
+cout<<a/b;
 
+}
+catch(int e)
+{
+    cout<<"Denominator can not be Zero\n";
+}
+cout<<"Done";
 }
