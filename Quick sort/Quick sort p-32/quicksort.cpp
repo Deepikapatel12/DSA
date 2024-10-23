4f44c4397a393eef8548da8ecc3ba9c591223131
Deepikapatel <deepikapatel5687@gmail.com>
2024-10-23 20:56:53 +0530

    Quick sort code

---
+++ Quick sort/Quick sort p-32/quicksort.cpp
@@ -0,0 +1,8 @@
+//Approach: Divide and conquer
+//Stable or not: not stable
+// inplace : yes
+//space complxcity: o(1)
+//time compexity: o(n log n)
+//best and average case time complexity : o(n log n)
+//wrost case time complexity: m o(n2)
+// structure based or value based: both
