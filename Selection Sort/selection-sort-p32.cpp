b9f0323e56e5641d70267de912f3b5d8de80f30f
Deepikapatel <deepikapatel5687@gmail.com>
2024-10-24 21:21:40 +0530

    Selection sort code

---
+++ Selection Sort/selection-sort-p32.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,5,7,9};
+    int chotu;
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+    cout<<"\n";
+    for(int i=0;i<n;i++)
+    {
+        chotu=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if
+        }
+    }
+}
